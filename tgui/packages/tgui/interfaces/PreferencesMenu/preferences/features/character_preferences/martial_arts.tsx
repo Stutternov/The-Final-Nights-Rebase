dfec964055d5b61a361ae2b7a94ecab3bb2c439b
Stutternov <47883419+Stutternov@users.noreply.github.com>
2026-05-01 19:43:00 -0400

    Tribe Adjustment

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/martial_arts.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/martial_arts.tsx
@@ -5,7 +5,7 @@ import { FeatureDropdownInput } from '../dropdowns';
 export const martial_arts_choice: FeatureChoiced = {
     name: 'Martial Arts Style',
     description:
-	'Due to your ability in brawling, you picked up a martial art of your choice! Select one from the list of choices. (Kailando, a Garou unique, can be found in merits)',
+	'Due to your ability in brawling, you picked up a martial art of your choice! Select one from the list of choices. (Kailindo, a Garou unique, can be found in merits)',
     component: FeatureDropdownInput,
 };
 
